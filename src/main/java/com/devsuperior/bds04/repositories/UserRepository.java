ac7179ee7c6760e7146853d63006baef72672557
DanielAssisDev <danielassisdeveloper@tutamail.com>
2026-09-14 00:57:03 -0300

    Adicionando a interface UserDetailsProjection, implementando o método de busca de usuário por email em UserRepository, e loadUserByUsername em UserService

--- src/main/java/com/devsuperior/bds04/repositories/UserRepository.java
+++ src/main/java/com/devsuperior/bds04/repositories/UserRepository.java
@@ -1,14 +1,20 @@
 package com.devsuperior.bds04.repositories;
 
 import com.devsuperior.bds04.entities.User;
+import com.devsuperior.bds04.projections.UserDetailsProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     @Query(nativeQuery = true, value = """
-            
+            SELECT u.email Username, u.password Password, r.id RoleId, r.authority Authority
+            FROM tb_user u INNER JOIN tb_user_role ur ON u.id=ur.user_id
+            INNER JOIN tb_role r ON ur.role_id=r.id
+            WHERE u.email= :email
             """)
     List<UserDetailsProjection> searchUserByUsernameAndRole(String email);
 }
--- src/main/java/com/devsuperior/bds04/services/UserService.java
+++ src/main/java/com/devsuperior/bds04/services/UserService.java
@@ -1,15 +1,36 @@
 package com.devsuperior.bds04.services;
 
+import com.devsuperior.bds04.entities.Role;
+import com.devsuperior.bds04.entities.User;
+import com.devsuperior.bds04.projections.UserDetailsProjection;
+import com.devsuperior.bds04.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UserService implements UserDetailsService {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        List<UserDetailsProjection> users = userRepository.searchUserByUsernameAndRole(username);
+        if (users.isEmpty()) {
+            throw new UsernameNotFoundException("Usuário não encontrado");
+        }
+        User user = new User();
+        user.setEmail(users.getFirst().getUsername());
+        user.setPassword(users.getFirst().getPassword());
+//        for(UserDetailsProjection udp : users){
+//            user.addRole(new Role(udp.getRoleId(), ));
+//        }
+
         return null;
     }
 }
