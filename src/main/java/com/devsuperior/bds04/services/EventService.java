a802e16f37849652bad89a5ec41f98c5dabf9d8a
DanielAssisDev <danielassisdeveloper@tutamail.com>
2026-09-13 23:56:37 -0300

    Criando a classe EventService

---
+++ src/main/java/com/devsuperior/bds04/services/EventService.java
@@ -0,0 +1,35 @@
+package com.devsuperior.bds04.services;
+
+import com.devsuperior.bds04.dto.EventDTO;
+import com.devsuperior.bds04.entities.Event;
+import com.devsuperior.bds04.repositories.CityRepository;
+import com.devsuperior.bds04.repositories.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class EventService {
+    @Autowired
+    private EventRepository eventRepository;
+
+    @Autowired
+    private CityRepository cityRepository;
+
+    @Transactional(readOnly = true)
+    public Page<EventDTO> findAll(Pageable pageable) {
+        return eventRepository.findAll(pageable).map(EventDTO::new);
+    }
+
+    @Transactional
+    public EventDTO insert(EventDTO eventDTO){
+        Event event = new Event();
+        event.setName(eventDTO.getName());
+        event.setDate(eventDTO.getDate());
+        event.setUrl(eventDTO.getUrl());
+        event.setCity(cityRepository.getReferenceById(eventDTO.getId()));
+        return new EventDTO(eventRepository.save(event));
+    }
+}
